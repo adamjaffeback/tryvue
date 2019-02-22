792cc1c65650f416fa51ef1685e69fa3d207e53f
Adam Jaffe Back <adamback42@gmail.com>
2019-02-22 15:27:14 -0800

    (feat) Create todos app.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!',
-    title: 'Hover to pop',
+    newTodo: undefined,
+    todos: [],
+  },
+  methods: {
+    addTodo: function () {
+      console.log(this);
+      this.todos.push(this.newTodo);
+      this.newTodo = undefined;
+    },
+    remove: function (event) {
+      this.todos = this.todos.filter(todo => todo !== event.target.innerText);
+    },
   },
 });
